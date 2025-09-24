052c0170f42e2fd6535f2f88a07ef571c1d203d5
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2025-09-24 23:20:03 +0300

    commit

--- TestDemo/Controllers/WeatherForecastController.cs
+++ TestDemo/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TestDemo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing","Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;

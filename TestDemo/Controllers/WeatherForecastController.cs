41ca2fbd0fd306ae5d454b5569bb53450e88f583
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2025-09-24 23:42:14 +0300

    commit

--- TestDemo/Controllers/WeatherForecastController.cs
+++ TestDemo/Controllers/WeatherForecastController.cs
@@ -21,9 +21,9 @@ namespace TestDemo.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Summaries.Select(index => new WeatherForecast
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(2)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })

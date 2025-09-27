fb66fab41b99cf3f0f4d4eec1a7eef966a196153
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2025-09-27 19:24:24 +0300

    commit

--- TestDemo/Program.cs
+++ TestDemo/Program.cs
@@ -9,17 +9,12 @@ namespace TestDemo
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             var app = builder.Build();
             app.UseSwagger();
             app.UseSwaggerUI();
-
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
             app.MapControllers();
-
             app.Run();
         }
     }
